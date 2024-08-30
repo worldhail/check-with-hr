e74fb52c1240bf01b42967f143362f7e03a1a7ba
Jeric Chua <worldhail41@gmail.com>
2024-08-30 22:08:24 +0800

    Add earning object endpoint for modification

--- server/models/payslip.js
+++ server/models/payslip.js
@@ -23,57 +23,60 @@ const hourlyType = [
 ];
 
 // SCHEMA
-const hourlyBreakdown = new mongoose.Schema({
+const earningsSchema = new mongoose.Schema({
+    'Earnings from Hours Worked': { type: Number, default: 0 },
+    'Performance Bonus / Attendance bonus': { type: Number, default: 0 },
+    'Other Earnings / Relocation / Referral': { type: Number, default: 0 },
+    'Total Earnings': { type: Number, default: 0 },
+},);
+
+const contriAndDeductSchema = new mongoose.Schema({
+    'Pag-IBIG': { type: Number, default: 0 },
+    'SSS': { type: Number, default: 0 },
+    'Philhealth': { type: Number, default: 0 },
+    'BIR Withholding Tax': { type: Number, default: 0 },
+    'SSS Loan Repayment': { type: Number, default: 0 },
+    'Pagibig Loan Repayment': { type: Number, default: 0 },
+    'Other Deductions': { type: Number, default: 0 },
+    'Total Contributions & Deductions': { type: Number, default: 0 },
+});
+
+const allowanceSchema = new mongoose.Schema({
+    'Rice Allowance': { type: Number, default: 0 },
+    'Laundry Allowance': { type: Number, default: 0 },
+    'Medical Cash Allowance': { type: Number, default: 0 },
+    'Uniform Allowance': { type: Number, default: 0 },
+    'Employee Pag-IBIG share paid by Smiles': { type: Number, default: 0 },
+    'Employee Philhealth share paid by Smiles': { type: Number, default: 0 },
+    '13th Month': { type: Number, default: 0 },
+    'Complexity Pay': { type: Number, default: 0 },
+    'Other Allowances': { type: Number, default: 0 },
+    'Total Allowances': { type: Number, default: 0 },
+});
+
+const hourlyBreakdownSchema = new mongoose.Schema({
     'Hour Type': { type: String, enum: hourlyType },
     'Hours': { type: Number, default: 0 },
     'Earnings': { type: Number, default: 0 }
 }, { _id: false });
 
 const payslipSchema = new mongoose.Schema({
-    'Earnings': {
-        'Earnings from Hours Worked': { type: Number, default: 0 },
-        'Performance Bonus / Attendance bonus': { type: Number, default: 0 },
-        'Other Earnings / Relocation / Referral': { type: Number, default: 0 },
-        'Total Earnings': { type: Number, default: 0 },
-    },
-    'Contributions & Deductions': {
-        'Pag-IBIG': { type: Number, default: 0 },
-        'SSS': { type: Number, default: 0 },
-        'Philhealth': { type: Number, default: 0 },
-        'BIR Withholding Tax': { type: Number, default: 0 },
-        'SSS Loan Repayment': { type: Number, default: 0 },
-        'Pagibig Loan Repayment': { type: Number, default: 0 },
-        'Other Deductions': { type: Number, default: 0 },
-        'Total Contributions & Deductions': { type: Number, default: 0 },
-    },
-    'Allowances': {
-        'Rice Allowance': { type: Number, default: 0 },
-        'Laundry Allowance': { type: Number, default: 0 },
-        'Medical Cash Allowance': { type: Number, default: 0 },
-        'Uniform Allowance': { type: Number, default: 0 },
-        'Employee Pag-IBIG share paid by Smiles': { type: Number, default: 0 },
-        'Employee Philhealth share paid by Smiles': { type: Number, default: 0 },
-        '13th Month': { type: Number, default: 0 },
-        'Complexity Pay': { type: Number, default: 0 },
-        'Other Allowances': { type: Number, default: 0 },
-        'Total Allowances': { type: Number, default: 0 },
-    },
-    'Hourly Breakdown': [ hourlyBreakdown ],
+    'user': { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    'Earnings': { type: earningsSchema, default: () => ({}) },
+    'Contributions & Deductions': { type: contriAndDeductSchema, default: () => ({}) },
+    'Allowances': { type: allowanceSchema, default: () => ({}) },
+    'Hourly Breakdown': { type: [ hourlyBreakdownSchema ] },
     'Totals': {
         'Hours': { type: Number, default: 0 },
         'Net Earnings': { type: Number, default: 0 },
         'Earnings Excluding Performance Bonus': { type: Number, default: 0 }
-    }
-});
-
-payslipSchema.pre('save', function () {
-    const sum = this['Hourly Breakdown'].reduce((a, b) => a['Hours'] + b['Hours'], 0);
-    this['Net Earnings'] = sum;
+    },
+    'date': { type: Date, default: Date.now }
 });
 
 const Payslip = mongoose.model('Payslip', payslipSchema);
 
 module.exports = {
     hourlyType,
-    Payslip
+    Payslip,
 }
--- server/protected-admin-routers/payslip.js
+++ server/protected-admin-routers/payslip.js
@@ -2,23 +2,111 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
+const debugAdmin = require('debug')('app:admin');
 
 // CUSTOM MODULES/MIDDLEWARES
-const { Payslip, hourlyType } = require('../models/payslip');
+const { hourlyType, Payslip } = require('../models/payslip');
 
-// ROUTER
-router.post('/payslip-template', async (req, res, next) => {
-    const breakdown = [];
-    for (let i = 0; hourlyType.length > i; i++) {
-        breakdown.push({ ['Hour Type']: hourlyType[i] });
+// PAYSLIP JOI SCHEMA AND ITS FUNCTION
+function schemaValidator(schema, info) {
+    const result = schema.validate(info, { abortEarly: false });
+    return result;
+};
+
+const earningSchema =  Joi.object({
+    'Earnings': Joi.object({
+        'Earnings from Hours Worked': Joi.number().default(0),
+        'Performance Bonus / Attendance bonus': Joi.number().default(0),
+        'Other Earnings / Relocation / Referral': Joi.number().default(0)
+    })
+});
+
+const hourlyBreakdownSchema = Joi.object({
+    'Hourly Breakdown': {
+        'Hour Type': Joi.string(),
+        'Hours': Joi.number().default(0),
+        'Earnings': Joi.number().default(0)
     }
+});
 
-    // const sum = this['Hourly Breakdown'].reduce((a, b) => a['Hours'] + b['Hours'], 0);
-    // this['Net Earnings'] = sum;
+// HELPER FUNCTIONS
+// Setting the properties of the nested object with dot notation
+function makeDottedKeyPairs(reqBody, objectName) {
+    // convert the object key value pairs in an array
+    const objValue = Object.entries(reqBody[objectName]);
+    
+    // connect the nested object property to it's child properties with dot notation on a string
+    const keyPairs = objValue.map(([keys, values]) => [`${objectName}.${keys}`, values]);
+    return keyPairs;
+};
+
+// sum of the nested objects from the saved values and new input values
+function getTotal(reqBody, objectName, savedObject) {
+    // convert the object key value pairs in an array
+    const reqBodykeyPairs = Object.entries(reqBody[objectName]);
+    const saveObjectKeyPairs = Object.entries(savedObject._doc);
+
+    // filter out the properties that are not going to be updated
+    // and push them to an array with the properties that will be updated
+    const unchangedValues =  saveObjectKeyPairs
+        .filter(([keys, values]) => keys !== `Total ${objectName}` && keys !== '_id')
+        .filter(([keys, values]) => !reqBody[objectName].hasOwnProperty(keys))
+        .forEach(items => reqBodykeyPairs.push(items));
+    
+    // get the values of the properties and add them up
+    const values = reqBodykeyPairs.map(([keys, values]) => values);
+    const sum = Number(values.reduce((acc, curr) => acc + curr, 0).toFixed(2));
+    return sum;
+};
+
+// ROUTERS
+// CREATING A PAYLISP TEMPLATE
+router.post('/payslip-template/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const savedPayslip = await Payslip.findOne({ user: id });
+        if (savedPayslip) return res.send(savedPayslip);
+        else {
+            // make an instance of the hourly type for the hourly breakdown
+            const breakdown = [];
+            for (let i = 0; hourlyType.length > i; i++) {
+                breakdown.push({'Hour Type': hourlyType[i]})
+            }
+
+            const payslip = new Payslip({ user: id, 'Hourly Breakdown': breakdown });
+            await payslip.save();
+            res.status(201).send(payslip);
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+// EDITTING PAYSLIP TEMPLATE FOR EMPLOYEES BREAKDOWN
+router.put('/payslip/earnings/:id', async (req, res, next)=> {
+    const id = req.params.id;
+    
+    // validate the input object and its properties
+    const { error } = schemaValidator(earningSchema, req.body);
+    if (error) return res.status(400).send(error.details.map(items => items.message));
     
     try {
-        const paySlip = new Payslip({ ['Hourly Breakdown']: breakdown });
-        res.send(paySlip);
+        const earningObject = await Payslip.findOne({ user: id }).select('Earnings -_id');
+        const earningId = earningObject['Earnings']._id;
+        if (!earningObject) return res.status(400).send('Payslip not found for the user');
+
+        // add all the property values of the Earning object
+        const sum = getTotal(req.body, 'Earnings', earningObject['Earnings']);
+
+        // set a dot notation on a string type for the key pairs and push the total property
+        const dottedPairs = makeDottedKeyPairs(req.body, 'Earnings');
+        dottedPairs.push(['Earnings.Total Earnings', sum]);
+
+        // convert the dottenPairs array to an object and update the properties
+        const newValues = Object.fromEntries(dottedPairs);
+        const updateEarnings = await Payslip.updateOne({ user: id, 'Earnings._id': earningId }, { $set: newValues });
+        debugAdmin(updateEarnings);
+        res.send(updateEarnings);
     } catch (error) {
         next(error);
     }
