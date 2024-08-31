4a380e37e82f1ca6107ac8128061bbefe51379a2
Jeric Chua <worldhail41@gmail.com>
2024-08-31 19:05:58 +0800

    Add contributions and deductions endpoint

--- server/protected-admin-routers/payslip.js
+++ server/protected-admin-routers/payslip.js
@@ -21,6 +21,18 @@ const earningSchema =  Joi.object({
     })
 });
 
+const contriAndDeductSchema = Joi.object({
+    'Contributions & Deductions': Joi.object({
+        'Pag-IBIG': Joi.number().default(0),
+        'SSS': Joi.number().default(0),
+        'Philhealth': Joi.number().default(0),
+        'BIR Withholding Tax': Joi.number().default(0),
+        'SSS Loan Repayment': Joi.number().default(0),
+        'Pagibig Loan Repayment': Joi.number().default(0),
+        'Other Deductions': Joi.number().default(0)
+    })
+});
+
 const hourlyBreakdownSchema = Joi.object({
     'Hourly Breakdown': {
         'Hour Type': Joi.string(),
@@ -44,7 +56,7 @@ function makeDottedKeyPairs(reqBody, objectName) {
 function getTotal(reqBody, objectName, savedObject) {
     // convert the object key value pairs in an array
     const reqBodykeyPairs = Object.entries(reqBody[objectName]);
-    const saveObjectKeyPairs = Object.entries(savedObject._doc);
+    const saveObjectKeyPairs = Object.entries(savedObject[objectName]._doc);
 
     // filter out the properties that are not going to be updated
     // and push them to an array with the properties that will be updated
@@ -82,7 +94,7 @@ router.post('/payslip-template/:id', async (req, res, next) => {
     }
 });
 
-// EDITTING PAYSLIP TEMPLATE FOR EMPLOYEES BREAKDOWN
+// EDITTING PAYSLIP TEMPLATE FOR EARNINGS CATEGORY
 router.put('/payslip/earnings/:id', async (req, res, next)=> {
     const id = req.params.id;
     
@@ -96,7 +108,7 @@ router.put('/payslip/earnings/:id', async (req, res, next)=> {
         if (!earningObject) return res.status(400).send('Payslip not found for the user');
 
         // add all the property values of the Earning object
-        const sum = getTotal(req.body, 'Earnings', earningObject['Earnings']);
+        const sum = getTotal(req.body, 'Earnings', earningObject);
 
         // set a dot notation on a string type for the key pairs and push the total property
         const dottedPairs = makeDottedKeyPairs(req.body, 'Earnings');
@@ -107,6 +119,37 @@ router.put('/payslip/earnings/:id', async (req, res, next)=> {
         const updateEarnings = await Payslip.updateOne({ user: id, 'Earnings._id': earningId }, { $set: newValues });
         debugAdmin(updateEarnings);
         res.send(updateEarnings);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// EDITTING PAYSLIP TEMPLATE FOR CONTRIBUTION AND DEDUCTION CATEGORY
+router.put('/payslip/contributions-and-deductions/:id', async (req, res, next)=> {
+    const id = req.params.id;
+    
+    // validate the input object and its properties
+    const { error } = schemaValidator(contriAndDeductSchema, req.body);
+    if (error) return res.status(400).send(error.details.map(items => items.message));
+    
+    try {
+        const contriAndDeduct = await Payslip.findOne({ user: id }).select({ 'Contributions & Deductions': 1, _id: 0 });
+        const contriAndDeductId = contriAndDeduct['Contributions & Deductions']._id;
+        debugAdmin('idasdf', contriAndDeduct, contriAndDeductId);
+        if (!contriAndDeduct) return res.status(400).send('Payslip not found for the user');
+
+        // add all the property values of the Earning object
+        const sum = getTotal(req.body, 'Contributions & Deductions', contriAndDeduct);
+
+        // set a dot notation on a string type for the key pairs and push the total property
+        const dottedPairs = makeDottedKeyPairs(req.body, 'Contributions & Deductions');
+        dottedPairs.push(['Contributions & Deductions.Total Contributions & Deductions', sum]);
+
+        // convert the dottenPairs array to an object and update the properties
+        const newValues = Object.fromEntries(dottedPairs);
+        const updateContriAndDeduct = await Payslip.updateOne({ user: id, 'Contributions & Deductions._id': contriAndDeductId }, { $set: newValues });
+        debugAdmin(updateContriAndDeduct);
+        res.send(updateContriAndDeduct);
     } catch (error) {
         next(error);
     }
