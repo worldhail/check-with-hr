6422c26cc3d4d2918186f7fce8565375d33e60a1
Jeric Chua <worldhail41@gmail.com>
2024-09-16 20:19:53 +0800

    interchange one conditional statement

--- server/public-routers/login.js
+++ server/public-routers/login.js
@@ -17,8 +17,9 @@ router.post('/user', async (req, res, next) => {
     // if user exist, compare password then autheniticates
     try {
         const user = await User.findOne({ email: req.body.email });
-        if (user.role !== req.body.role) return res.status(400).send('Invalid email or password');
+        debugUser('asdf', user)
         if (!user) return res.status(400).send('Invalid email or password');
+        if (user.role !== req.body.role) return res.status(400).send('Invalid email or password');
 
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).send('Invalid email or password');
