5fceb2f141898014c5c982247f2ab33a4f3b48bd
Jeric Chua <worldhail41@gmail.com>
2024-10-23 21:48:56 +0800

    Refactor the login endpoint

--- server/public-routers/login.js
+++ server/public-routers/login.js
@@ -1,39 +1,33 @@
 // NPM PACKAGES
 import express from 'express';
 const router = express.Router();
-import bcrypt from 'bcrypt';
 import debug from 'debug';
 const debugUser = debug('app:user');
 
 // CUSTOM MODULES/MIDDLEWARES
-import User from '../models/user.js';
 import authLoginSchema from '../joi-schema-validator/authLoginSchema.js';
+import validate from '../middleware/validate.js';
+import getUser from '../services/getUser.js';
+import comparePassword from '../services/comparePassword.js';
 
 // POST - USER LOGIN
-router.post('/user', async (req, res) => {
-    // validate user credentials
-    const { error } = authLoginSchema.validate(req.body, { abortEarly: false });
-    if (error) return res.status(400).send(error.details[0].message);
-
+export default router.post('/user', validate(authLoginSchema), async (req, res) => {
     // if user exist, compare password then autheniticates
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send('Invalid email or password');
-    if (user.role !== req.body.role) return res.status(400).send('Invalid email or password');
+    const authorizedUser = await getUser({ email: req.body.email });
+    if (!authorizedUser) return res.status(400).send('Invalid email or password');
+    if (authorizedUser.role !== req.body.role) return res.status(400).send('Invalid email or password');
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password');
+    const isValidPassword = await comparePassword(req.body.password, authorizedUser.password);
+    if (!isValidPassword) return res.status(400).send('Invalid email or password');
     
     // // create a token for the user
-    const token = user.generateAuthToken();
+    const token = authorizedUser.generateAuthToken();
     res.cookie('x-auth-token', token, {
         httpOnly: true,
         secure: false,
         sameSite: 'lax',
     });
 
-    // const to_roleHomePage = user.role === 'admin' ? '/api/admin/profile' : '/api/user/profile';
-    debugUser('...Logging in to ', user.role);
+    debugUser('...Logging in to ', authorizedUser.role);
     res.redirect('/api/account-routes/profile');
 });
-
-export default router;
