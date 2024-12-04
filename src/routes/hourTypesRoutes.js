87cc0f46c92ba204c709cfccae6de65679eca030
Jeric Chua <worldhail41@gmail.com>
2024-12-04 20:29:55 +0800

    Upadate the hourType controller for the new filename set

--- src/routes/hourTypesRoutes.js
+++ src/routes/hourTypesRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-import addHourType from "../controllers/addHourType.js";
+import addHourTypeController from "../controllers/addHourTypeController.js";
 import hourTypeSchema from "../joi-schema-validator/hourTypeSchema.js";
 import validate from "../middleware/validate.js";
 
-export default router.post('/hour-type', validate(hourTypeSchema), addHourType);
+export default router.post('/hour-type', validate(hourTypeSchema), addHourTypeController);
