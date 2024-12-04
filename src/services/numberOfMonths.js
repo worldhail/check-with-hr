382d7c797efe8cf89d01523659d9a3da0b10f869
Jeric Chua <worldhail41@gmail.com>
2024-12-04 20:30:41 +0800

    Wrap regularization date to new Date method

--- src/services/numberOfMonths.js
+++ src/services/numberOfMonths.js
@@ -1,5 +1,5 @@
 export default function (data) {
-    const timeDifference = new Date() - data.regularizationDate;
+    const timeDifference = new Date() - new Date(data.regularizationDate);
     const days = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
     const months = Math.floor(days / 30);
 
